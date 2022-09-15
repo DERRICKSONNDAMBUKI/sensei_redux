86a6fbcd1702bebcf4cc99f4031d6f9cd40471bf
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-15 16:36:45 +0300

    combineReducers commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,11 @@
+import { combineReducers } from 'redux'
 import todosReducer from './features/todos/todosSlice'
 import filtersReducer from './features/filters/filtersSlice'
 
-export default rootReducer = (state = {}, action) => {
-  // always return anew object for the root state
-  return {
-    todos: todosReducer(state.todos, action),
-    filters: filtersReducer(state.filters, action),
-  }
-}
+const rootReducer = combineReducers({
+  // Define a top-level state field named `todos`, handled by `todosReducer`
+  todos: todosReducer(state.todos, action),
+  filters: filtersReducer(state.filters, action),
+})
+
+export default rootReducer
