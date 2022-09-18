c551db4c93978c4ca4107652aee4723e601a6c15
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-09-18 14:34:43 +0300

    redux thunk commit

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import {
   includeMeaningOfLife,
   sayHiOnDispatch,
 } from './exampleAddons/enhancers'
 import { print1, print2, print3 } from './exampleAddons/middleware'
 import rootReducer from './reducer'
+import ThunkMiddleware from 'redux-thunk'
 
 // let preloadState
 // const persistedTodosString = localStorage.getItem('todos')
@@ -16,13 +18,16 @@ import rootReducer from './reducer'
 // }
 // const store = createStore(rootReducer, preloadState)
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+// const middlewareEnhancer = applyMiddleware(print1, print2, print3)
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife, middlewareEnhancer)
+// const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife, middlewareEnhancer)
+
 // Pass enhancer as the second arg, since there's no preloadedState
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(ThunkMiddleware))
 const store = createStore(
   rootReducer,
-//   middlewareEnhancer
+  //   middlewareEnhancer
   composedEnhancer
 )
 
